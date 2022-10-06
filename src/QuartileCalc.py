8fd4aa320a90b92a03fd9faafaa8e3fa02a45276
IL0veHub <motys2013@yandex.ru>
2022-10-06 14:37:58 +0300

    Test 2

--- src/QuartileCalc.py
+++ src/QuartileCalc.py
@@ -1,8 +1,3 @@
 class QuartileCalc:
-    def __init__(self,rating):
+    def __init__(self, rating):
         self.rating = rating
-
-    def calc(self):
-    #if (50.0<=self.rating<=75.0):
-            print("Rating: ", self.rating)
-
